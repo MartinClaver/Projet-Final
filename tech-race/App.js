16924a1154b3d8b62d27ae2cd93867ad92d2606b
Martin Claver <m.claver99@gmail.com>
2024-06-30 14:21:36 +0200

    route navigation update

--- tech-race/App.js
+++ tech-race/App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import EndedRace from './src/EndedRace';
 
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Go to EndedRace page"
+        onPress={() => navigation.navigate('EndedRace')}
+      />
     </View>
   )
 }
