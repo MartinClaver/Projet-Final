c8ad85fe3a59a97c9ac5f8e7ffd3089e0b59ec50
Martin Claver <m.claver99@gmail.com>
2024-06-30 14:11:39 +0200

    route navigation

--- tech-race/App.js
+++ tech-race/App.js
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import EndedRace from './src/EndedRace';
 
-export default function App() {
+
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="EndedRace" component={EndedRace}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
