227897ca62fae8dbbc74ee267eae61b89bf70e41
Martin Claver <m.claver99@gmail.com>
2024-07-01 14:32:40 +0200

    landscape mode and svg

--- tech-race/screens/EndedRace.js
+++ tech-race/screens/EndedRace.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import HomeLogo from "../assets/HomeLogo.svg";
+import { useEffect } from 'react';
+import Svg, { Defs, LinearGradient, Stop, start, Circle } from 'react-native-svg';
+import HomeLogo from "../assets/Group1.svg";
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 const EndedRace = () => {
+  useEffect(() => {
+    const lockOrientation = async () => {
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    };
+  
+    lockOrientation();
+  
+    return () => {
+      ScreenOrientation.unlockAsync();
+    };
+  }, []);
   return (
     <View style={styles.container}>
-      <HomeLogo width={80} height={80}/>
+      {/* <HomeLogo width={80} height={80} fill='white'/> */}
+      <Svg height="120" width="120" style={styles.svg}>
+        <Defs>
+          <LinearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="100%">
+            <Stop offset="0%" stopColor={start ? "#FF6F61": "#00C99C"} stopOpacity="1" />
+            <Stop offset="100%" stopColor={start ? "#B22222": "#00634D" } stopOpacity="1" />
+          </LinearGradient>
+        </Defs>
+        <Circle cx="60" cy="60" r="55" stroke="url(#grad)" strokeWidth="5" fill="none" />
+      </Svg>
       <Text>RACE SUMMARY</Text>
-      {/* <Image source={require('../assets/carImage.png')} width={120} height={120} /> */}
+      <Image source={require('../assets/carImage.png')} style={styles.carImage}/>
       <View style={styles.statsContainer}>
       <Text>Timer</Text>
       </View>
@@ -40,8 +63,8 @@ const styles = StyleSheet.create({
     height: '80px',
   },
   carImage: {
-    width: '258px',
-    height: '258px',
+    width: '120px',
+    height: '120px',
   },
 });
 
