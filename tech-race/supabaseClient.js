4ce563098c8ed40cb9421fcf3e10280384aa73d1
clementmurca <clem.murca@gmail.com>
2024-09-04 09:24:52 +0200

    Ajout de supabaseClient.js au .gitignore

--- tech-race/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://grtbnpwowoilrfabehxm.supabase.co/';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdydGJucHdvd29pbHJmYWJlaHhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc1NzM5ODYsImV4cCI6MjAzMzE0OTk4Nn0.slTAAHR9WjvgZhxnxVxgd9TsMqmqHpBmFvdrheSOnLs';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
